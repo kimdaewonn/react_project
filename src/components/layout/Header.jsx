8a3fceadc0485138648f1738802771aba5ab72ef
kimdaewonn <eodnjs9605@gmail.com>
2023-06-03 00:38:26 +0900

    link

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -15,31 +15,21 @@ const Header = () => {
               <img src="../assets/orange-one.png" alt="오렌지" />
               오늘의 레시피
             </li>
-            {/* <li>
-            <Link to="Main">
-              <button>main</button>
-            </Link>
-          </li> */}
             <li>
-              <Link to="class">
+              <Link to="/class">
                 <button>CLASSIFICATION</button>
               </Link>
             </li>
             <li>
-              <Link to="ranking">
+              <Link to="/ranking">
                 <button>RANKING</button>
               </Link>
             </li>
             <li>
-              <Link to="youtube">
+              <Link to="/youtube">
                 <button>YOUTUBE</button>
               </Link>
             </li>
-            {/* <li>
-            <Link to="recipes">
-              <button>Recipes</button>
-            </Link>
-          </li> */}
           </ul>
         </div>
       </article>
